796ab640a1c2f3a5a5751e476e3c883c508ffd40
Princess-Jewel Essien <princess-jewel.essien@andela.com>
2018-03-12 23:31:49 +0100

    Fix SQL errors

--- tools/proxy.js
+++
@@ -1,8 +0,0 @@
-#!/usr/env/node
-const path = require('path');
-const childProcess = require('child_process');
-const exec = childProcess.execSync;
-const execScript = childProcess.execFileSync;
-
-execScript(path.join(__dirname, 'proxy_setup.sh'));
-exec('./cloud_sql_proxy -dir=/cloudsql &');
--- tools/proxy.sh
+++ tools/proxy.sh
@@ -1,4 +1,5 @@
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 chmod +x cloud_sql_proxy
-sudo mkdir /cloudsql
+sudo mkdir -p /cloudsql
 sudo chmod 777 /cloudsql
+./cloud_sql_proxy -dir=/cloudsql &
