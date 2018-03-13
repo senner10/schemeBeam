93648d3d686ac7746a7cd8fc2b175e728c7932f5
Princess-Jewel Essien <princess-jewel.essien@andela.com>
2018-03-13 01:48:31 +0100

    Change background image

--- config/adminconfig.js
+++ config/adminconfig.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   //Port on which to deploy app.js
-  port: 80,
+  port: 8080,
   //Your personal SendGrid API key. 
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   //The id of the list you would like your client emails to (a number, can be found at the end the url path of your list)
