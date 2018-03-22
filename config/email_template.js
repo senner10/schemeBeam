1242dcd32b5ddfd52f4bc95ba5545be0f556cfd6
senner10 <alex@orkiv.com>
2018-03-22 11:41:29 -0500

    Ommited sensitive files

--- config/email_template.js
+++ config/email_template.js
@@ -1,4 +1,4 @@
-module.exports =
+cmodule.exports =
 function(domain, hashCode, color, emailImage, preHeader, sender, emailHeader, footerName, footerLocation) {
 	return (
  '   <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">  '  + 
--- config/settingsconfig.js
+++ config/settingsconfig.js
@@ -1,27 +1,26 @@
 module.exports = {
-//range of top referrers that you intend on rewarding
-  prizeRange: 50,
-//title on the sign up page. change to the name of the company or something relevant to the campaign
-  landerPageTitle: "schemeBeam",
-//message that will populate on the sign up page
-  landerPageMessage: "The top 50 referrers will recieve a free copy of our new marketing software and a key to our early-access beta test and support forum. " +
-  "Enter your email to receive your referral link and start sharing with your friends and colleagues!",
+	//range of top referrers that you intend on rewarding
+	prizeRange: 5000,
+	//title on the sign up page. change to the name of the company or something relevant to the campaign
+	landerPageTitle: "ALCOA APP REFERRAL",
+	//message that will populate on the sign up page
+	landerPageMessage: "Join below and have a chance to be within the first group of 5,000 people who will all get $10 in their account to start. ONE referral equals ONE jump in line. Top 10 - $100, Top 50 - $75 Top 100 - $50. GOOD LUCK ;-)",
 
-//EMAIL SETTINGS
-//email theme color, set to match your personal branding
-  brandColor: "#05abe0",
-//path to image used in email. width is responsive and will fit to the formatting automatically. leave blank if you don't want to include one.
-  emailImage: "http://obeymarketinggroup.com/wp-content/uploads/2016/08/Does-Inbound-Marketing-Work.png",
-//email address that you would like the sender to be labeled as
-  senderAddress: "schemebeam@schemebeam.com",
-//email subject line
-  subjectLine: "Your referral code is here!",
-//comes after the subject line when email is displayed in inbox
-  preHeader: "Start climbing your way to the top now!",
-//header of the email. either your company name or any other greeting you might find relevant
-  emailHeader: "schemeBeam",
-//First line of the footer. Put in your business name or whatever you feel is appropriate
-  footerName: "schemeBeam",
-//Second line of the footer. Put in your business location or whatever you feel is appropriate
-  footerLocation: "Phoenix, AZ"
-}
+	//EMAIL SETTINGS
+	//email theme color, set to match your personal branding
+	brandColor: "#81cc61",
+	//path to image used in email. width is responsive and will fit to the formatting automatically. leave blank if you don't want to include one.
+	emailImage: "http://35.188.159.174/wp-content/uploads/2018/02/cropped-Alcoa-Logo_inverse-1.png",
+	//email address that you would like the sender to be labeled as
+	senderAddress: "referral@getalcoa.com",
+	//email subject line
+	subjectLine: "Your Alcoa App referral code is here!",
+	//comes after the subject line when email is displayed in inbox
+	preHeader: "Refer and Earn Cash",
+	//header of the email. either your company name or any other greeting you might find relevant
+	emailHeader: "GET ALCOA",
+	//First line of the footer. Put in your business name or whatever you feel is appropriate
+	footerName: "Alcoa App",
+	//Second line of the footer. Put in your business location or whatever you feel is appropriate
+	footerLocation: "Boston",
+};
